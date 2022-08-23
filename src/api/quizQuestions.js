78231742502baad54eb9c98f000ff8dd7351d692
Julius Goddard <julius.goddard@webforprofessionals.com>
2022-08-23 15:39:30 +0100

    nightmare

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,89 @@
+var quizQuestions = [
+  {
+      question: "When your friends change restaurants for dinner at the last minute do you think....?",
+      answers: [
+          {
+              type: "Stick with your current employee job",
+              content: "How rude!"
+          },
+          {
+              type: "You’re got potential",
+              content: "Whatever"
+          },
+          {
+              type: "Get launching!  You’re an Entrepreneur.",
+              content: "A new place, cool!"
+          }
+      ]
+  },
+  {
+      question: "When your mentor says, why don't you try for the Most Awesome Award, you'll be the first person from your state to win...Do you think..?",
+      answers: [
+          {
+              type: "Stick with your current employee job",
+              content: "No way this is happening."
+          },
+          {
+              type: "You’re got potential",
+              content: "Okay, maybe this will work."
+          },
+          {
+              type: "Get launching!  You’re an Entrepreneur.",
+              content: "Absolutely! I'm on it."
+          }
+      ]
+  },
+  {
+      question: "Someone walks up to you on the street and says, I'll give you $25 to balance this brick on top of these 10 coffee stir sticks. Do you say..?",
+      answers: [
+          {
+              type: "Stick with your current employee job",
+              content: "I think you've mistaken me for Harry Potter."
+          },
+          {
+              type: "You’re got potential",
+              content: "I need coffee money hand over the sticks and I'll see what happens."
+          },
+          {
+              type: "Get launching!  You’re an Entrepreneur.",
+              content: "I need coffee money hand over the sticks and I'll see what happens."
+          }
+      ]
+  },
+  {
+      question: "When your bank calls and says, we haven't received a paycheck deposit for you this month do you...?",
+      answers: [
+          {
+              type: "Stick with your current employee job",
+              content: "Break out in a cold sweat"
+          },
+          {
+              type: "You’re got potential",
+              content: "Run from the room screaming"
+          },
+          {
+              type: "Get launching!  You’re an Entrepreneur.",
+              content: "Write code while eating dinner at your desk to complete a new app"
+          }
+      ]
+  },
+  {
+      question: "When your top client calls to complain that your company's dog collar fell off letting his prize bulldog run onto the highway and he's cancelling his order, your accountant calls to ask about a missing invoice and your website crashes in a Russian cyber attack, do you:",
+      answers: [
+          {
+              type: "Stick with your current employee job",
+              content: "Run screaming into the sidewalk pulling out your hair."
+          },
+          {
+              type: "You’re got potential",
+              content: "Sigh and remind yourself how much you love learning new stuff while setting up a key team members meeting to investigate."
+          },
+          {
+              type: "Get launching!  You’re an Entrepreneur.",
+              content: "Call your recruiter friend and ask whether your old employer is hiring."
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
